a5ce4b1341eb13ebade08f3aefe9e7e8d5518f59
EMERSON ROCHA DE OLIVEIRA <37919193850@jpmorgan.sp>
2018-01-24 20:39:56 -0200

    botão direito

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,71 @@
-var lista = [];
+var lista = localStorage.getItem("notas") ? JSON.parse(localStorage.getItem("notas")) : [];
+
+document.oncontextmenu = new Function ("return false");
+console.dir(document);
+
+if (lista.length > 0) {
+    atualizarNotas(lista, document.getElementById("sessao-notas"))
+}
+
 //https://nettuts.s3.amazonaws.com/771_sticky/step5.html
 function atualizarNotas(notas, sessao) {
+    localStorage.setItem("notas", JSON.stringify(notas));
     sessao.innerHTML = '';
     for (var c = 0; c < notas.length; c++) {
-        var cor = style='style="background-color:'+ notas[c].cor +';"'
+        var cor = style = 'style="background-color:' + notas[c].cor + ';"'
         sessao.innerHTML +=
-            '<form class="note" '+cor+'>' +
-            '<button '+cor+' class="note__control" type="button" onclick="onRemoveClick(this.form.parentElement, ' + c + ')">' +
+            '<form class="note" ' + cor + ' ondblclick="editarNota(' + c + ', this.parentElement.previousElementSibling)" oncontextmenu="editarNotaLocal(' + c + ', this)">' +
+            '<button ' + cor + ' class="note__control" type="button" onclick="onRemoveClick(this.form.parentElement, ' + c + ')">' +
             '<i class="fa fa-times" aria-hidden="true"></i>' +
             '</button>' +
-            '<input '+cor+' class="note__title" value="' + notas[c].titulo + '" disabled ></input>' +
-            '<textarea  '+cor+' class="note__body" disabled>' + notas[c].body + '</textarea>' +
+            '<input ' + cor + ' class="note__title" name="title" type="text" value="' + notas[c].titulo + '" disabled ></input>' +
+            '<textarea  ' + cor + ' class="note__body" name="body" disabled>' + notas[c].body + '</textarea>' +
+            '<input type="color" name="cor" value="' + notas[c].cor + '" class="oculto">'+
+            '<button class="note__control oculto" type="button" onclick="adicionarNota(this.form, true, '+c+')">Concluído</button>'+
             '</form>';
     }
 }
 
-function adicionarNota(form) {
+function adicionarNota(form, is_editar, index_editar) {
+    console.log(form, is_editar, index_editar);
+    console.dir(form);
     //criar variavel nota
     var nota = {
         titulo: form.title.value,
         body: form.body.value,
         cor: form.cor.value
     };
-    console.log(nota);
+    console.log(nota, is_editar);
     //adicionar nota dentro da lista
-    lista.push(nota);
-    // limpar formulario
-    atualizarNotas(lista, form.nextElementSibling)
+    if (is_editar) {
+        is_editar = false;
+        lista[index_editar] = nota;
+        atualizarNotas(lista, form.parentElement)
+    } else {
+        lista.push(nota);
+        atualizarNotas(lista, form.nextElementSibling)
+    }
+    // limpar formulario   
     form.reset();
 }
 
-function onRemoveClick(sessao, index){
+function onRemoveClick(sessao, index) {
+    console.log(lista);
     console.log(sessao, index);
     lista.splice(index, 1);
     console.log(lista);
-    atualizarNotas(lista, sessao)   
+    atualizarNotas(lista, sessao)
+}
+
+function editarNota(index, form) {    
+    form.title.value = lista[index].titulo;
+    form.body.value = lista[index].body
+    form.cor.value = lista[index].cor    
+}
+
+function editarNotaLocal(index, form){    
+    form.cor.className = "";
+    form.lastChild.className = "note__control";
+    form.title.disabled = false;
+    form.body.disabled = false;        
 }
